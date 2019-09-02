dbff2a585e8b0be48ec7eb50c08b238c445e6c41
Alexander Bispo <alexbispo173@gmail.com>
2019-09-02 16:54:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,14 @@ def reduce_to_total(source_array,starting_point=0)
 end
 
 def reduce_to_all_true(source_array)
+  count = 0
   
+  while count < source_array.length do
+      
+    count += 1
+  end
+  
+  return 
 end
 
 
